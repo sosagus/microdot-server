20cf7d0940729d4f2c4a780596b44f0ac76be43d
sosagus <sosagustavoar@gmail.com>
2024-09-09 13:22:06 -0300

    feat: actividad 1

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,18 @@
 # Aplicacion del servidor
+from microdot import Microdot, send_file
+
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+@app.route('/<dir>/<file>')
+async def index(request, dir, file):
+    return send_file("/{}/{}".format(dir, file))
+
+@app.route('/<img>')
+async def index(request, img):
+    return send_file("/{}".format(img))
+
+app.run(port=80)
--- actividades/ej_01/boot.py
+++ actividades/ej_01/boot.py
@@ -1,1 +1,13 @@
 # Configuracion inicial
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('ENTRANDO...')
+        sta_if.active(True)
+        sta_if.connect('Cooperadora Alumnos', '')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+    
+do_connect()
